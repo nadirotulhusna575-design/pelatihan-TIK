b16a6c21ce07524796cf9d848ac2803b439cff9c
nadirotulhusna575-design <nadirotulhusna575@gmail.com>
2025-10-23 10:16:33 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// 1.pilih tombol berdasarkan ID-nya
+const tombolsapa = dokumen .getElementBYID('sapa Button');
+//2.Tambahkan 'event listener 'untuk aksi 'click'
+tombolsapa.addventlistener('click'function() {
